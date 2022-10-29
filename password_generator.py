26f72e5f99e8d84fe53d373dc194b95fc03f7173
Nathan Metens <83681521+metens@users.noreply.github.com>
2022-10-28 22:38:50 -0700

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,27 +1,28 @@
-# Create a random password that is impossible to crack :) in py 
+# Create a random password that is impossible to crack :) in python.
 
 import random
 import math
 
-alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-symbols = '~!@#$%^&*()_+=-`<>,./?;:\][{}'
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' # List of all the upper/lowercase letters in the alphabet
+symbols = '~!@#$%^&*()_+=-`<>,./?;:\][{}' # A bunch of symbols from the keyboard
 psswd_len  = 20; # password length
 
 password = ''
 
 for x in range(psswd_len):
 
-	char_choice = math.floor(random.random() * 3); # will be random between the sym, alph, and nums
-
-	if char_choice == 0:
-		random_char = str(math.floor(random.random() * 10))
-
-	elif char_choice == 1:
-		random_char = symbols[math.floor(random.random() * len(symbols))]
-
-	else:
-		random_char = alphabet[math.floor(random.random() * len(alphabet))] 
-
+	char_choice = math.floor(random.random() * 3); # A random number. Either 0, 1, or 2
+	
+	# Depending on the char_choice, either alphabet, symbol, or number will be the next character in the password
+	
+	match char_choice:
+		case 0:
+			random_char = str(math.floor(random.random() * 10))
+		case 1:
+			random_char = symbols[math.floor(random.random() * len(symbols))]
+		case 2:
+			random_char = alphabet[math.floor(random.random() * len(alphabet))]
+	
 	password += random_char;
 
 print(password);
